79eb0d03d3a2fd91c13fa0d9d6f05334de2799d5
francisco <fharavena@gmail.com>
2020-02-06 19:46:48 -0300

    coneccion a dbb y creacion de entidades

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,16 +3,54 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use App\Entity\Video;
 
 class UserController extends AbstractController
 {
+    private function resjson($data){
+        // Serializar datos con servicio de serializer
+        $json = $this->get('serializer')->serialize($data, 'json');
+
+        // response con httpfoundation
+        $response = new Response();
+
+        // asignar contenido a la respuesta
+        $response->setContent($json);
+
+        // indicar formato de respuesta
+        $response->headers->set('Content-Type', 'application/json');
+
+        // devolver la respuesta
+        return $response;
+    }
+
 
     public function index()
     {
-        return $this->json([
+        $user_repo = $this->getDoctrine()->getRepository(User::class);
+        $video_repo = $this->getDoctrine()->getRepository(Video::class);
+
+        $users = $user_repo->findAll();
+        $user = $user_repo->find(1);
+        $videos = $video_repo->findAll();
+
+
+        $data = [
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
-        ]);
+        ];
+
+        // foreach($users as $user){
+        //     echo "<h1>{$user->getName()} {$user->getSurname()}</h1>";
+        //     foreach ($user->getVideos() as $video) {
+        //         echo "<p> {$video->getTitle()} - {$video->getUser()->getEmail()}</p>";
+
+        //     }
+        // }
+
+        return $this->resjson($data);
     }
 }
